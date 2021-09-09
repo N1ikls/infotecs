8fc60f546010f915b57a851699a69dfea10a3caf
N1ikls <sau3000@mail.ru>
2021-09-09 16:11:25 +0300

    js add

---
+++ js.js
@@ -0,0 +1,126 @@
+"use strict";
+async function JSON() {
+  let url = "http://localhost:3000/JSON";
+  let response = await fetch(url);
+  let json = await response.json(); // читаем ответ в формате JSON
+  console.log(json); // проверяем получили ли ответ
+
+  let count = 10; // количество страниц выведенных
+  let table = document.querySelector("#table");
+  let items = [];
+  let pag = document.querySelector("#pagination");
+
+  for (let i = 1; i <= Math.ceil(json.length / count); i++) {
+    // формула с учетом сколько нужно номеров (пагинации) для count
+    let li = document.createElement("li"); // например 50/10 = 5 - номеров для 10 страниц
+    li.innerHTML = i;
+    pag.appendChild(li);
+    items.push(li);
+  }
+
+  let s = document.querySelector("#sort");
+  for (let i = 1; i <= 4; i++) {
+    // просто выведем лист чтобы по нажатию сортировали
+    let li = document.createElement("p");
+    li.innerHTML = `Sort  ${i} column`;
+    s.appendChild(li);
+  }
+
+  showPage(items[0]); // Чтобы отображалось хотябы 1 (пагинация )
+  
+  let active; // стиль чтобы знать на каком номере мы остановились
+  for (let item of items) {
+    item.addEventListener("click", function () {
+      // функция клика по номеру (чтобы узнавать на какой номер мы нажали)
+      if (active) {
+        active.classList.remove("active");
+      }
+      active = this;
+      this.classList.add("active");
+      showPage(this);
+    });
+  }
+
+  let t = document.querySelector("#table");
+  let trs = document.querySelectorAll("#table tr");
+  let button = document.querySelectorAll("#sort p");
+  for (let i in button) {
+    // сортировка по кнопкам
+    button[i].addEventListener("click", () => {
+      let sorted = [...trs].sort((a, b) => {
+        if (a.children[i].innerHTML >= b.children[i].innerHTML) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+      t.innerHTML = " ";
+      for (let tr of sorted) {
+        t.appendChild(tr);
+      }
+    });
+  }
+
+  function showPage(item) {
+    // вывод таблицы
+    // функция пагинации
+    let pag = +item.innerHTML; // какой номер мы кликнули
+    let notes = [];
+    //let start = (pag - 1) * count;
+    //let end = start + count;   пытался сделать так. Но почему то не хочет рабоать прорешал на листке все сходится :(
+    //notes = json.splice(start,end)
+
+    for (let i in json) {
+      notes.push(json[i]);
+    }
+    if (pag == 1) {
+      notes = notes.slice(0, 10);
+    }
+    if (pag == 2) {
+      notes = notes.slice(10, 20);
+    }
+    if (pag == 3) {
+      notes = notes.slice(20, 30);
+    }
+    if (pag == 4) {
+      notes = notes.slice(30, 40);
+    }
+    if (pag == 5) {
+      notes = notes.slice(40, 50);
+    }
+
+    table.innerHTML = "";
+
+    for (let note in notes) {
+      // выводим таблицу привязанные к пагинации
+      let tr = document.createElement("tr");
+      table.appendChild(tr);
+      let td;
+
+      td = document.createElement("td");
+      td.innerHTML = notes[note].name.firstName;
+      tr.appendChild(td);
+
+      td = document.createElement("td");
+      td.innerHTML = notes[note].name.lastName;
+      tr.appendChild(td);
+
+      td = document.createElement("td");
+      td.innerHTML = notes[note].about;
+      tr.appendChild(td);
+
+      td = document.createElement("td");
+      td.className += `color_${note}`;
+      td.innerHTML = ' ';
+      tr.appendChild(td);
+      console.log(note)
+    }
+    for (let note in notes) { // чтобы поменять цвета в блоке
+      let square = document.querySelector(`.color_${note}`);
+      square.style.backgroundColor = `${notes[note].eyeColor}`;
+      square.style.width = `70px`;
+      }
+  }
+  
+}
+JSON(); // запуск функции
